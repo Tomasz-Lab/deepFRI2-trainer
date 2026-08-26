b4ce4b6fefc80e294d0fda349c7e605da416221e
PawelSzczerbiak <p.szczerbiak@gmail.com>
2026-08-26 09:50:39 +0000

    Add running script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate deepfri2_trainer
+
+# Train models for all antologies from the scratch with default parameters
+# python train.py --ontology MF
+# python train.py --ontology CC
+# python train.py --ontology BP
